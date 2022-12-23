e53cb58f14b2f66a4a178aa8372d1ea398adce12
Marco Zuñiga <alex13_hbk@hotmail.com>
2022-12-23 17:08:58 -0600

    Add files and first challenge

---
+++ E1.cs
@@ -0,0 +1,30 @@
+﻿namespace cs_exercises
+{
+    internal class E1
+    {
+        static void Main(string[] args)
+        {
+            FindGreatestNumber();
+        }
+
+        static void FindGreatestNumber()
+        {
+            //Find biggest number
+
+            Console.WriteLine("Enter first number");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            double n2 = double.Parse(Console.ReadLine());
+
+            if (n1 != n2)
+            {
+                Console.WriteLine($"the biggest number is  {(n2 > n1 ? n2 : n1)}");
+            }
+            else
+            {
+                Console.WriteLine("Same number");
+            }
+        }
+    }
+}
